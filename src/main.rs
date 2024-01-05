9b0a352545c0c99020101f84e8fa78890c0591ff
Sathish Kannan <sathishkannan162@gmail.com>
2024-01-06 03:40:24 +0530

    Complete a simple employee data input

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,75 @@
 use core::panic;
-use std::{collections::HashMap, io};
+use std::{
+    collections::HashMap,
+    io::{self, stdin},
+};
+
+enum YesNo {
+    Yes,
+    No,
+}
 
 fn main() {
     let departments = vec!["Engineering", "Sales", "Information technology"];
+    let mut department_employee_map: HashMap<&str, Vec<String>> = HashMap::new();
+    println!("Enter the data of the Employees");
+    loop {
+        let mut name_input = String::new();
+        println!("Enter Employee Name: ");
+        io::stdin()
+            .read_line(&mut name_input)
+            .expect("failed to read line");
+        let name = name_input.trim();
+        println!("Which departments do you want to assign to");
+        for (index, department) in departments.iter().enumerate() {
+            println!("{}: {department}", index + 1);
+        }
+        let mut department_number_input = String::new();
+        io::stdin()
+            .read_line(&mut department_number_input)
+            .expect("failed to read line");
+        println!("department number input: {department_number_input}");
+        let department_number: usize = department_number_input
+            .trim()
+            .parse()
+            .expect("Enter a valid number");
 
-    let mut name_input = String::new();
-    println!("Enter Employee Name: ");
-    io::stdin()
-        .read_line(&mut name_input)
-        .expect("failed to read line");
-    let name = name_input.trim();
-    println!("Which departments do you want to assign to");
-    for (index, department) in departments.iter().enumerate() {
-        println!("{}: {department}", index + 1);
+        if department_number > departments.len() {
+            panic!("Enter a valid department number");
+        }
+        let department = departments
+            .get(department_number - 1)
+            .expect("Enter a valid department_number");
+
+        let temp_vector: Vec<String> = Vec::new();
+        let mut current_vector = department_employee_map
+            .get(department)
+            .unwrap_or(&temp_vector)
+            .clone();
+        current_vector.push(name.to_string());
+        let value = department_employee_map.insert(&department, current_vector);
+        println!("department values {:?}", department_employee_map);
+        let mut choice_input = String::new();
+        println!("Do you want to continue? (y or n):");
+        io::stdin()
+            .read_line(&mut choice_input)
+            .expect("failed to read line.");
+        let choice = match choice_input.trim().to_lowercase().as_str() {
+            "y" => YesNo::Yes,
+            "n" => YesNo::No,
+            _ => {
+                println!("Invalid choice, defaulting to No.");
+                YesNo::No
+            }
+        };
+        if let YesNo::No = choice {
+            break;
+        }
+        // below match statement is also works.
+        // match choice {
+        //     YesNo::Yes => (),
+        //     YesNo::No => break,
+        // }
     }
-    let mut department_number_input = String::new();
-    io::stdin()
-        .read_line(&mut department_number_input)
-        .expect("failed to read line");
-    let department_number: usize = department_number_input
-        .trim()
-        .parse()
-        .expect("Enter a valid number");
-
-    // if department_number > convert_usize_i8(departments.len()) {
-    //     panic!("Enter a valid department number");
-    // }
-    let department = departments
-        .get(department_number - 1)
-        .expect("Enter a valid department_number");
-
-    let mut department_employee_map: HashMap<&str, Vec<&str>> = HashMap::new();
-    let current_vector = department_employee_map.get(department);
-    println!("{:?}", current_vector);
-    let value = department_employee_map.insert(&department, current_vector]);
-    println!("{:?}", department_employee_map);
-}
-
-fn convert_usize_i8(u: usize) -> i8 {
-    if u <= i8::MAX as usize {
-        return u as i8;
-    } else {
-        return 0;
-    }
+    
 }
